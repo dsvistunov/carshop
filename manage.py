65d2ba59fd4c0feff91e71b96eb74ea17987f058
dsvistunov <d.svistuno0v1991@gmail.com>
2017-03-20 22:40:40 +0200

    commit

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
 if __name__ == "__main__":
+    #load dotenv
+    dotenv.read_dotenv()
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
     try:
