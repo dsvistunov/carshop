fa450b2a3b6076931a7a13a14ab5c4ade2607cdc
dsvistunov <d.svistuno0v1991@gmail.com>
2018-08-03 20:57:18 +0300

    managing media on dev

--- src/urls.py
+++ src/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+
+from . import settings
+
 
 urlpatterns = [
     url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^', include('cars.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
